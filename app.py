9f41ee76d4b991ff124c386d622b58f5299109e3
Neva <neva.burges@accrete.ai>
2022-06-18 20:04:02 +0530

    image changes

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
 
 @app.route("/glenburn")
 def glenburn():
-    df = pd.read_csv("data/glenburnfinetea-final2.csv", index_col=0, encoding= 'unicode_escape')
+    df = pd.read_csv("data/glenburn.csv", index_col=0, encoding= 'unicode_escape')
     df = df.fillna(0)
     items = df.to_dict('records')
     return render_template('glenburn.html', items=items)
@@ -29,12 +29,20 @@ def tata():
     return render_template('tata.html', items=items)
 
 
-@app.route("/spice")
-def spice():
-    df = pd.read_csv("data/spice.csv", index_col=0)
+@app.route("/weaveskart")
+def weavekart():
+    df = pd.read_csv("data/weaveskart-new.csv", index_col=0)
     df = df.fillna(0)
     items = df.to_dict('records')
     return render_template('spice.html', items=items)
+
+
+@app.route("/prideofindia")
+def pride():
+    df = pd.read_csv("data/prideofindia.csv", index_col=0)
+    df = df.fillna(0)
+    items = df.to_dict('records')
+    return render_template('prideofindia.html', items=items)
 
 
 if __name__ == '__main__':

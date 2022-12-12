03060b12a796bad8c40f07a09a802204b799efb4
NevaBurges <neva.burges@accrete.ai>
2022-12-12 19:05:22 +0530

    test changes

--- app.py
+++ app.py
@@ -9,42 +9,47 @@ app = Flask(__name__)
 def index():
     df = pd.read_csv("data/tdt-new.csv", index_col=0)
     df = df.fillna(0)
-    items = df.to_dict('records')
-    return render_template('main.html', items=items)
+    items = df.to_dict("records")
+    return render_template("main.html", items=items)
 
 
 @app.route("/glenburn")
 def glenburn():
-    df = pd.read_csv("data/glenburn-tea.csv", encoding= 'unicode_escape')
+    df = pd.read_csv("data/glenburn-tea.csv", encoding= "unicode_escape")
     df = df.fillna(0)
-    items = df.to_dict('records')
-    return render_template('glenburn.html', items=items)
+    items = df.to_dict("records")
+    return render_template("glenburn.html", items=items)
 
 
 @app.route("/tata")
 def tata():
     df = pd.read_csv("data/tata.csv", index_col=0)
     df = df.fillna(0)
-    items = df.to_dict('records')
-    return render_template('tata.html', items=items)
+    items = df.to_dict("records")
+    return render_template("tata.html", items=items)
 
 
 @app.route("/weaveskart")
 def weavekart():
     df = pd.read_csv("data/weaveskart.csv", index_col=0)
     df = df.fillna(0)
-    items = df.to_dict('records')
-    return render_template('spice.html', items=items)
+    items = df.to_dict("records")
+    return render_template("spice.html", items=items)
 
 
 @app.route("/prideofindia")
 def pride():
     df = pd.read_csv("data/prideofindia.csv", index_col=0)
     df = df.fillna(0)
-    items = df.to_dict('records')
-    return render_template('prideofindia.html', items=items)
+    items = df.to_dict("records")
+    return render_template("prideofindia.html", items=items)
 
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
     app.run(debug=True, port=5001)
 
